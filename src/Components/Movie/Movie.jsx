b12f5e75e99a521d17ab79a9af108145491465d4
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-11-12 09:15:49 +0300

    N25 add favourite star in details card

--- src/Components/Movie/Movie.jsx
+++ src/Components/Movie/Movie.jsx
@@ -4,15 +4,23 @@ import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import CardMedia from "@mui/material/CardMedia";
 import CircularProgress from "@mui/material/CircularProgress";
-import { useSelector } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import { PrimaryButton } from "../UI/PrimaryButton";
 import { useHistory } from "react-router-dom";
+import StarsIcon from "@mui/icons-material/Stars";
+import { setFavouriteAsync } from "../../Thunks";
 
 export const Movie = () => {
   const { getDetails, isFavourites } = useSelector((state) => state.movies);
+  const {currentUser} = useSelector(state=> state.user);
+  const sessionId = localStorage.getItem('session_id');
   const history = useHistory();
+  const dispatch = useDispatch();
+  const onClickFavourite = () => {
+    dispatch(setFavouriteAsync(sessionId,currentUser.id,getDetails.id));
+  };
   return (
-    <Box sx={{ width: 900, m: "0 auto", position: 'relative' }}>
+    <Box sx={{ width: 900, m: "0 auto", position: "relative" }}>
       {isFavourites ? (
         <PrimaryButton
           onClick={() => history.push(`/favourites`)}
@@ -49,40 +57,51 @@ export const Movie = () => {
             </Typography>
           </Box>
           <Box
+            sx={{
+              right: 50,
+              position: "absolute",
+              borderRadius: "50%",
+              display: "inline-flex",
+              bgcolor: "black",
+            }}
+          >
+            {isFavourites ? '': <StarsIcon sx={{ fontSize: 40, color: "white" , cursor: 'pointer'}} onClick={onClickFavourite}/>}
+          </Box>
+          <Box
+            sx={{
+              right: 0,
+              position: "absolute",
+              borderRadius: "50%",
+              display: "inline-flex",
+              bgcolor: "black",
+            }}
+          >
+            <CircularProgress
+              variant="determinate"
+              color="success"
+              value={getDetails.vote_average * 10}
+            />
+            <Box
               sx={{
-                right:0,
+                top: 0,
+                left: 0,
+                bottom: 0,
+                right: 0,
                 position: "absolute",
-                borderRadius: "50%",
-                display: "inline-flex",
-                bgcolor: "black",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
             >
-              <CircularProgress
-                variant="determinate"
-                color="success"
-                value={getDetails.vote_average * 10}
-              />
-              <Box
-                sx={{
-                  top: 0,
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  position: "absolute",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
+              <Typography
+                variant="caption"
+                component="div"
+                sx={{ color: "white" }}
               >
-                <Typography
-                  variant="caption"
-                  component="div"
-                  sx={{ color: "white" }}
-                >
-                  {`${getDetails.vote_average * 10}%`}
-                </Typography>
-              </Box>
+                {`${getDetails.vote_average * 10}%`}
+              </Typography>
             </Box>
+          </Box>
         </Box>
       </Paper>
     </Box>
