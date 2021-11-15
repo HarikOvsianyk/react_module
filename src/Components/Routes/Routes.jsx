40c310ffaca92255f76c0df59aaca4478260fd60
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-11-15 16:01:45 +0300

    N45 try to fix routes

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { allRoutes } from "./allRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { MainPage } from "../Pages/MainPage";
+import { LogInPage } from "../Pages/LogInPage";
+import { SignUpPage } from "../Pages/SignUpPage";
+import { ErrorPage } from "../Pages/ErrorPage";
+import { ProfilePage } from "../Pages/ProfilePage";
+import { MoviePage } from "../Pages/MoviePage";
+import {FavouritesPage} from '../Pages/FavouritesPage';
 
 export const Routes = () => {
   return (
     <Switch>
-      {allRoutes.map((route, i) =>
-        route.isPrivate ? (
-          <PrivateRoutes
-            key={i}
-            path={route.path}
-            component={route.component}
-          />
-        ) : (
-          <Route key={i} path={route.path} component={route.component} exact />
-        )
-      )}
-
+      <Route exact path= {"/"} component={LogInPage} />
+      <Route  path= {"/main"} component={MainPage} />
+      <Route  path= {"/profile"} component={ProfilePage} />
+      <Route  path= {"/movie/:id"} component={MoviePage} />
+      <Route  path= {"/favourites"} component={FavouritesPage} />
+      <Route  path= {"/registration"} component={SignUpPage} />
+      <Route  path= {"/error"} component={ErrorPage} />
       <Redirect to="/error" />
     </Switch>
   );
