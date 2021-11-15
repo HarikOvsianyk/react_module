020deff192c5a0afb698eb46748893772fcdfde5
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-11-15 15:47:51 +0300

    N43 change folder

--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import { allRoutes } from "../../AllRoutes/allRoutes";
+import { allRoutes } from "./allRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
 
 export const Routes = () => {
